91f2eaf87274250084de1a25d301784535ef6c56
Samet Aslan <sametaslan1710@gmail.com>
2024-05-10 01:34:19 +0300

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -9,23 +9,89 @@ test("normalizeURL strip protocol", () => {
 });
 
 test("normalizeURL strip trailing slash", () => {
-    const input = "https://facebook.com/profiles/";
-    const actual = normalizeURL(input);
-    const expected = "facebook.com/profiles";
+  const input = "https://facebook.com/profiles/";
+  const actual = normalizeURL(input);
+  const expected = "facebook.com/profiles";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://FACEBOOK.com/profiles/";
+  const actual = normalizeURL(input);
+  const expected = "facebook.com/profiles";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://facebook.com/profiles/";
+  const actual = normalizeURL(input);
+  const expected = "facebook.com/profiles";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://facebook.com/path/">
+            Facebook Blog
+            </a>
+        </body>
+    </html>
+  `;
+  const inputBaseURL = "https://facebook.com/path/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://facebook.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+    const inputHTMLBody = `
+      <html>
+          <body>
+              <a href="/path/">
+              Facebook Blog
+              </a>
+          </body>
+      </html>
+    `;
+    const inputBaseURL = "https://facebook.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://facebook.com/path/"];
     expect(actual).toEqual(expected);
   });
-  
-  test("normalizeURL capitals", () => {
-    const input = "https://FACEBOOK.com/profiles/";
-    const actual = normalizeURL(input);
-    const expected = "facebook.com/profiles";
+
+  test("getURLsFromHTML both", () => {
+    const inputHTMLBody = `
+      <html>
+          <body>
+              <a href="https://facebook.com/path1/">
+              Facebook Blog Path 1
+              </a>
+              <a href="/path2/">
+              Facebook Blog Path 2
+              </a>
+          </body>
+      </html>
+    `;
+    const inputBaseURL = "https://facebook.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://facebook.com/path1/","https://facebook.com/path2/"];
     expect(actual).toEqual(expected);
   });
-  
-  test("normalizeURL strip http", () => {
-    const input = "http://facebook.com/profiles/";
-    const actual = normalizeURL(input);
-    const expected = "facebook.com/profiles";
+
+  test("getURLsFromHTML invalid", () => {
+    const inputHTMLBody = `
+      <html>
+          <body>
+              <a href="invalid">
+              Invalid URL
+              </a>
+          </body>
+      </html>
+    `;
+    const inputBaseURL = "https://facebook.com";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
   });
-  
